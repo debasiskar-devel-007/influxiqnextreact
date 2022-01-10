b25ef9feb076b02b0bf406dac256eb37d8c74f7f
influxiq1 <himadribar.influxiq@gmail.com>
2022-01-10 16:58:29 +0530

    meta url modify

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
           height={250}
           className={classes.img}
         /></center>
-        <Typography variant='body1' className={classes.lorem}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</Typography>
+        <Typography variant='body1' className={classes.lorem}>{"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"}</Typography>
       </div>
 
     </Container>
