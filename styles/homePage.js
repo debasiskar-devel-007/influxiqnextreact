66afcf7e6b5916567a906ecd31f5982006d269d2
influxiq1 <himadribar.influxiq@gmail.com>
2022-01-10 16:51:03 +0530

    meta url modify

---
+++ styles/homePage.js
@@ -0,0 +1,23 @@
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+    wrap: {
+        backgroundColor: '#118086',
+        marginTop: 60,
+        marginBottom: 50
+    },
+    lorem: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginTop: 20,
+        color: 'white',
+        paddingBottom: 40
+    },
+    img: {
+        marginTop: 40,
+    }
+
+});
+
+// const classes = useStyles();
+export default useStyles;
